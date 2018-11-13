5dd0605f0254b04df778558c9777ad5497730551
Patriek van Dorp <padorp@microsoft.com>
2018-11-13 13:02:03 +0100

    add random word selector

--- src/Randomizer.cs
+++ src/Randomizer.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 
-namespace Random
+namespace Randomizer
 {
-    public class Randomizer
+    public class Generator
     {
-        
+        //RandomWord picks a random word from a line of text.
+        public static string RandomWord(string text) 
+        {
+            var words = text.Split("\\w");
+
+            var random = new Random((int)DateTime.UtcNow.Ticks);
+
+            var index = random.Next(words.Length);
+            
+            return words[index];
+        }
     }
 }
